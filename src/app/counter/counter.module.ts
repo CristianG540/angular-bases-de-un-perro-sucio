28231210b301331156a294e96355212d786a86f4
CristianG5401 <cristian.gv@imaginelearning.com>
2021-12-17 01:04:39 -0500

    feat: two-way binding agregado al contador usando ngModel

--- src/app/counter/counter.module.ts
+++ src/app/counter/counter.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 // Components
 import { CounterComponent } from './basic-counter/counter.component';
@@ -9,7 +10,10 @@ import { CounterComponent } from './basic-counter/counter.component';
   // En el imports va cualquier modulo que necesite usar El modulo
   // 'CommonModule' por ejemplo, me permite usar directivas como if, for en los
   // templates html
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule // Este modulo lo uso en este caso solo para usar two-way binding con ngModel
+  ],
   // En el exports se incluye todo lo que se usa fuera del modulo, es como crear
   // un metodo publico de una clase
   exports: [CounterComponent],
